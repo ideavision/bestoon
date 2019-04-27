a15e4ae693b683a191fdc54d63b675a450e5a887
Narvik Aghamalian <mygalexit@gmail.com>
2019-04-28 01:24:39 +0400

    Expense & Income class in progress

--- web/models.py
+++ web/models.py
@@ -7,3 +7,17 @@ class Expense(models.Model):
     date=models.DateTimeField()
     amount=models.BigIntegerField()
     user=models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return '{} - {}'.format(self.date,self.amount)
+        # return self.text
+
+
+class Income(models.Model):
+    text=models.CharField(max_length=255)
+    date=models.DateTimeField()
+    amount=models.BigIntegerField()
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return '{}-{}'.format(self.date,self.amount)
+
+    
